dd1abfd0148f8c62cb749869f8c96f20cd9fa2f9
nsomazr <zreuben815@gmail.com>
2026-06-30 13:35:29 +0300

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || (import.meta.env.DEV ? '/api' : 'https://api.st.nileagi.com/api');
+const API_URL = import.meta.env.VITE_API_URL || (import.meta.env.DEV ? '/api' : 'https://api.smarttravel.co.tz/api');
 
 export { API_URL };
 
